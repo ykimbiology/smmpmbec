a41345835d13eaabfec301db29217f98ccd617bf
Yohan <yohan@irt-Precision-WorkStation-T3500.(none)>
2013-11-22 11:06:48 -0800

    updated readme and code

--- data/README.md
+++ data/README.md
@@ -1,15 +1,17 @@
 
-This folder contains the two matrices of amino acid residue similarity:
+This folder contains the two matrices of amino acid residue similarity: PMBEC and BLOSUM62.
+
 
 1) cov_matrix.pmbec.mat
-    Covariance matrix from the mhc binding data: PMBEC
+    Covariance matrix from the MHC Position Scanning Combinatorial Library binding data: PMBEC
     dimensions: 20x20
 
 2) ncbi.blosum62.sij.mat
-    NCBI's BLOSUM62 matrix.
+    NCBI's BLOSUM62 matrix. The matrix was used as if it was a covariance matrix in the bayesion prior model.
     dimensions: 20x20
 
 3) InverseCovariance.txt
-    Inverse of the PMBEC covariance matrix.
-    Note: A constant of 0.05 was added to the matrix before taking the inverse.
+    Inverse of the PMBEC covariance matrix hardcoded in the c++ code.
+    Note: A constant of 0.05 was added to all 20x20=400 elements of the matrix before taking the inverse.
+    Note: This matrix is slightly different from the inverse of the matrix from 1). This difference appears to be due to slight differences in the PMBEC cov matrices.
     dimensions: 20x20
--- data/check_covariance_matrix.py
+++ data/check_covariance_matrix.py
@@ -1,6 +1,13 @@
 
+"""
+Show how the PMBEC covariance matrix was inverted.
+
+
+"""
+import math
 
 import numpy as np
+from scipy.stats import pearsonr
 
 import matplotlib.pyplot as plt
 
@@ -19,48 +26,87 @@ def read_matrix(fname, header=False):
     assert mat.shape == (20,20)
     return mat
 
-def flatten(m):
-    x=[]
-    (nrow,ncol) = m.shape
-    for i in range(nrow):
-        for j in range(ncol):
-            x.append(m[i,j])
-    return x
 
-if __name__ == '__main__':
-    fname_pmbec = 'cov_matrix.pmbec.mat'
-    fname_blosum = 'ncbi.blosum62.sij.mat'
-    fname_pmbec_inv = 'InverseCovariance.txt'
+def compare_eig(ma,mb):
+    """
+    Compare eigenvalues and eigenvectors of the two matrices.
+    """
+    wa,va = np.linalg.eig(ma)
+    wb,vb = np.linalg.eig(mb)
+
+    result = sorted(zip(wa,va.transpose()), key=lambda row:row[0])
+    row_list = [row[1] for row in result]
+    v = np.array(row_list)
+    va = v.transpose()
+
+    result = sorted(zip(wb,vb.transpose()), key=lambda row:row[0])
+    row_list = [row[1] for row in result]
+    v = np.array(row_list)
+    vb = v.transpose()
 
 
+    #Plot eigenvalues: Exclude one with zero eigenvalue.
+    plt.plot(sorted(wa)[1:], sorted(wb)[0:-1], 'ko')
+    x = [min(sorted(wa)[1:]), max(sorted(wa)[1:])]
+    plt.plot(x,x, '-')
+    plt.show()
+
+
+
+def analyze_pmbec(offset=0.05):
     m = read_matrix(fname_pmbec,header=True) 
     m_inv_orig = read_matrix(fname_pmbec_inv,header=False) 
 
     m_inv = np.linalg.inv(m)
 
-    mb = m + 0.04
+    mb = m + offset
+    #mb = m + np.eye(20)*offset  
     mb_inv = np.linalg.inv(mb)
 
-    mc = m + np.eye(20)*0.02
-    mc_inv = np.linalg.inv(mc)
-
-    xa = flatten(m_inv_orig)
-    xb = flatten(m_inv)
-    xc = flatten(mb_inv)
-    xd = flatten(mc_inv)
-
-    plt.subplot(1,3,1)
-    plt.plot(xa,xb, 'ko')
-
-    # inv(M) == inv(M + c)?
-    plt.subplot(1,3,2)
-    plt.plot(xa,xc, 'ko')
-
-    plt.subplot(1,3,3)
-    plt.plot(xa,xd, 'ko')
-
-
+    xref = m_inv_orig.flatten()
+    xa = m_inv.flatten()
+    xb = mb_inv.flatten()
+  
+    #Plot correlations:
+    #=================
+    plt.plot(xref,xb, 'ko')
+    xline = [min(xb),max(xb)]
+    plt.plot(xline,xline, 'r-')
+    plt.xlabel('Inverse cov matrix hardcoded in the c++ code')
+    plt.ylabel('Inverse cov matrix after adding 0.05 to all elements')
     plt.show()
+
+    print 'M = PMBEC covariance matrix.'
+    print 'det(M) = ', np.linalg.det(m)
+    print 'det(M + %s)'%(offset,), '=', np.linalg.det(mb)
+
+    print 'Correlation between the inverse matrix and pmbec w/ offset added:', pearsonr(xref, xb)[0]
+    return m
+
+def analyze_blosum():
+    m = read_matrix(fname_blosum, header=True)
+    m_inv = np.linalg.inv(m)
+    print 'M = BLOSUM62 matrix'
+    print 'det(M) ', np.linalg.det(m)
+    return m, m_inv
+
+def get_condition_number(m):
+    """
+    http://mathworld.wolfram.com/ConditionNumber.html
+    """
+    u,s,v = np.linalg.svd(m);
+    cond_num = max(s)/min(s)
+    return cond_num
+
+if __name__ == '__main__':
+    fname_pmbec = 'cov_matrix.pmbec.mat'
+    fname_blosum = 'ncbi.blosum62.sij.mat'
+    fname_pmbec_inv = 'InverseCovariance.txt' #This was used in the smmpmbec code.
+
+
+    m_inv = read_matrix(fname_pmbec_inv,header=False)
+    m = analyze_pmbec(offset=0.05)
+    #m, m_inv = analyze_blosum()
 
 
     
